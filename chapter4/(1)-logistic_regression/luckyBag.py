4bd7ea18e6286c23acb11f2bb74b019727ab6d5d
akm4545 <akm4545@naver.com>
2024-06-13 17:21:31 +0900

    chapter4

--- chapter4/(1)-logistic_regression/luckyBag.py
+++ chapter4/(1)-logistic_regression/luckyBag.py
@@ -91,3 +91,58 @@ print(train_target[indexes])
 
 # Roach가 1개이고 Perch가 2개다
 # 따라서 세 번째 클래스에 대한 확률은 2/3 = 0.6667 이고 다섯번째 클래스에 대한 확률은 1/3 = 0.3333이다
+
+# 로지스틱 회귀(logistic regression)
+# 이름은 회귀이지만 분류 모델이다
+# 이 알고리즘은 선형 회귀와 동일하게 선형 방정식을 삭습한다
+# 예시 z = a * (Weight) + b * (Length) + c * (Diagonal) + d * (Height) + e * (Width) + f
+# 여기서에 a,b,c,d,e는 가중치 혹은 계수이다 
+# 특성은 늘어났지만 다중 회귀를 위한 선형 방정식과 같다
+# z는 어떤 값도 가능하다 하지만 확률이 되려면 0 ~ 1 (또는 0 ~ 100%) 사이 값이 되어야 한다
+
+# z가 아주 큰 음수일 때 0이 되고 아주 큰 양수일 때 1이 되도록 바꾸려면
+# 시그모이드 함수(sigmoid function 또는 로지스틱 함수(logistic function))를 사용하면 가능하다
+# 선형 방정식의 출력 z의 음수를 사용해 자연 상수 e를 거듭제곱하고 1을 더한 값의 역수를 취한다
+# z가 무한하게 큰 음수일 경우 이 함수는 0에 가까워지고 
+# z가 무한하게 큰 양수가 될 때는 1에 가까워진다
+# z가 0이 될 때는 0.5가 된다
+# z가 어떤 값이 되더라도 ø는 절대로 0 ~ 1 사이의 범위를 벗어날 수 없다
+
+# -5와 5가 시그모이드 함수를 사용하면 값이 0 ~ 1 사이의 범위에서 생성되는지 테스트
+# 넘파이를 사용하여 시그모이드 함수 그래프 생성
+# -5와 5 사이에 0.1 간격으로 배열 z를 만든 다음 z위치마다 시그모이드 함수를 계산
+# 지수 함수 계산은 np.exp() 함수를 사용
+import numpy as np
+import matplotlib.pyplot as plt
+
+z = np.arange(-5, 5, 0.1)
+phi = 1 / (1 + np.exp(-z))
+plt.plot(z, phi)
+plt.xlabel('z')
+plt.ylabel('phi')
+plt.show()
+
+# 도미와 빙어로 이진 분류 수행
+# 시그모이드 함수의 출력이 0.5보다 크면 양성 클래스, 0.5보다 작으면 음성 클래스
+# 정확히 0.5일때는 라이브러리마다 다를 수 있다 사이킷런은 0.5일때 음성 클래스로 판단
+
+# 넘파이 배열은 True, False 값을 전달하여 행을 선택할 수 있다 이를 불리언 인덱싱(boolean indexing)이라고 한다
+# 불리언 인덱싱 예제
+char_arr = np.array(['A', 'B', 'C', 'D', 'E'])
+print(char_arr[[True, False, True, False, False]])
+# ['A', 'C']
+
+# 불리언 인덱싱을 사용하여 도미와 빙어의 행만 가져오기
+# Bream | Smelt면 True 아니면 False가 담긴 배열 리턴
+bream_smelt_indexes = (train_target == 'Bream') | (train_target == 'Smelt')
+train_bream_smelt = train_scaled[bream_smelt_indexes]
+target_bream_smelt = train_target[bream_smelt_indexes]
+
+# 로지스틱 회귀 모델은 LogisticRegression 클래스를 사용하며 선형 모델이므로 sklearn.linear_model 패키지 아래에 있다
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+lr.fit(train_bream_smelt, target_bream_smelt)
+
+# 예측
+print(lr.predict(train_bream_smelt[:5]))
+['Bream' 'Smelt' 'Bream' 'Bream' 'Bream']
