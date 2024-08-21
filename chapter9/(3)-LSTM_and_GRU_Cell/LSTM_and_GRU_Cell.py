43bd1731349f2f40e3be1f0b7ae57ed0b49afbfe
akm4545 <akm4545@naver.com>
2024-08-21 23:29:13 +0900

    chapter9

--- chapter9/(3)-LSTM_and_GRU_Cell/LSTM_and_GRU_Cell.py
+++ chapter9/(3)-LSTM_and_GRU_Cell/LSTM_and_GRU_Cell.py
@@ -84,3 +84,99 @@ model.summary()
 #  Total params: 0 (0.00 B)
 #  Trainable params: 0 (0.00 B)
 #  Non-trainable params: 0 (0.00 B)
+
+# 모델을 컴파일 하고 훈련
+# 배치 크기 64, 에포크 횟수 100
+# 체크포인트와 조기 종료
+rmsprop = keras.optimizers.RMSprop(learning_rate=1e-4)
+model.compile(optimizer=rmsprop, loss='binary_crossentropy', metrics=['accuracy'])
+checkpoint_cb = keras.callbacks.ModelCheckpoint('best-lstm-model.keras', save_best_only=True)
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=3, restore_best_weights=True)
+history = model.fit(train_seq, train_target, 
+    epochs=100, batch_size=64, 
+    validation_data=(val_seq, val_target),
+    callbacks=[checkpoint_cb, early_stopping_cb])
+# Epoch 1/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 17s 46ms/step - accuracy: 0.5282 - loss: 0.6928 - val_accuracy: 0.5744 - val_loss: 0.6919
+# Epoch 2/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 14s 45ms/step - accuracy: 0.5803 - loss: 0.6916 - val_accuracy: 0.5958 - val_loss: 0.6902
+# Epoch 3/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 21s 47ms/step - accuracy: 0.6090 - loss: 0.6894 - val_accuracy: 0.6216 - val_loss: 0.6867
+# Epoch 4/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 21s 48ms/step - accuracy: 0.6301 - loss: 0.6850 - val_accuracy: 0.6498 - val_loss: 0.6784
+# Epoch 5/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 19s 42ms/step - accuracy: 0.6536 - loss: 0.6734 - val_accuracy: 0.6890 - val_loss: 0.6498
+# Epoch 6/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 13s 41ms/step - accuracy: 0.7005 - loss: 0.6301 - val_accuracy: 0.7182 - val_loss: 0.5899
+# Epoch 7/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 14s 44ms/step - accuracy: 0.7264 - loss: 0.5805 - val_accuracy: 0.7310 - val_loss: 0.5679
+# Epoch 8/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 20s 42ms/step - accuracy: 0.7393 - loss: 0.5618 - val_accuracy: 0.7468 - val_loss: 0.5476
+# Epoch 9/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 22s 47ms/step - accuracy: 0.7500 - loss: 0.5436 - val_accuracy: 0.7510 - val_loss: 0.5342
+# Epoch 10/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 20s 44ms/step - accuracy: 0.7713 - loss: 0.5166 - val_accuracy: 0.7698 - val_loss: 0.5158
+# Epoch 11/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 14s 44ms/step - accuracy: 0.7729 - loss: 0.5100 - val_accuracy: 0.7696 - val_loss: 0.5047
+# Epoch 12/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 21s 45ms/step - accuracy: 0.7768 - loss: 0.4957 - val_accuracy: 0.7710 - val_loss: 0.4952
+# Epoch 13/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 21s 46ms/step - accuracy: 0.7866 - loss: 0.4812 - val_accuracy: 0.7838 - val_loss: 0.4825
+# Epoch 14/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 20s 44ms/step - accuracy: 0.7900 - loss: 0.4721 - val_accuracy: 0.7824 - val_loss: 0.4788
+# Epoch 15/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 21s 45ms/step - accuracy: 0.7963 - loss: 0.4640 - val_accuracy: 0.7886 - val_loss: 0.4663
+# Epoch 16/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 15s 47ms/step - accuracy: 0.8033 - loss: 0.4498 - val_accuracy: 0.7910 - val_loss: 0.4608
+# Epoch 17/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 20s 46ms/step - accuracy: 0.8041 - loss: 0.4439 - val_accuracy: 0.7920 - val_loss: 0.4547
+# Epoch 18/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 20s 45ms/step - accuracy: 0.8062 - loss: 0.4395 - val_accuracy: 0.7884 - val_loss: 0.4552
+# Epoch 19/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 21s 46ms/step - accuracy: 0.8055 - loss: 0.4362 - val_accuracy: 0.7888 - val_loss: 0.4510
+# Epoch 20/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 14s 44ms/step - accuracy: 0.8098 - loss: 0.4314 - val_accuracy: 0.7978 - val_loss: 0.4441
+# Epoch 21/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 20s 43ms/step - accuracy: 0.8132 - loss: 0.4253 - val_accuracy: 0.8000 - val_loss: 0.4441
+# Epoch 22/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 20s 43ms/step - accuracy: 0.8103 - loss: 0.4225 - val_accuracy: 0.7922 - val_loss: 0.4432
+# Epoch 23/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 21s 44ms/step - accuracy: 0.8110 - loss: 0.4243 - val_accuracy: 0.7996 - val_loss: 0.4384
+# Epoch 24/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 20s 44ms/step - accuracy: 0.8090 - loss: 0.4274 - val_accuracy: 0.7904 - val_loss: 0.4436
+# Epoch 25/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 20s 43ms/step - accuracy: 0.8185 - loss: 0.4121 - val_accuracy: 0.7986 - val_loss: 0.4350
+# Epoch 26/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 20s 43ms/step - accuracy: 0.8131 - loss: 0.4184 - val_accuracy: 0.7976 - val_loss: 0.4437
+# Epoch 27/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 21s 45ms/step - accuracy: 0.8138 - loss: 0.4166 - val_accuracy: 0.7982 - val_loss: 0.4333
+# Epoch 28/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 14s 44ms/step - accuracy: 0.8134 - loss: 0.4169 - val_accuracy: 0.7982 - val_loss: 0.4330
+# Epoch 29/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 21s 45ms/step - accuracy: 0.8150 - loss: 0.4137 - val_accuracy: 0.8004 - val_loss: 0.4325
+# Epoch 30/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 14s 44ms/step - accuracy: 0.8132 - loss: 0.4111 - val_accuracy: 0.7970 - val_loss: 0.4359
+# Epoch 31/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 20s 43ms/step - accuracy: 0.8137 - loss: 0.4125 - val_accuracy: 0.8018 - val_loss: 0.4313
+# Epoch 32/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 13s 43ms/step - accuracy: 0.8172 - loss: 0.4062 - val_accuracy: 0.7978 - val_loss: 0.4310
+# Epoch 33/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 13s 42ms/step - accuracy: 0.8169 - loss: 0.4061 - val_accuracy: 0.7930 - val_loss: 0.4342
+# Epoch 34/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 13s 42ms/step - accuracy: 0.8122 - loss: 0.4088 - val_accuracy: 0.7960 - val_loss: 0.4438
+# Epoch 35/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 14s 44ms/step - accuracy: 0.8172 - loss: 0.4045 - val_accuracy: 0.8032 - val_loss: 0.4293
+# Epoch 36/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 20s 43ms/step - accuracy: 0.8275 - loss: 0.3899 - val_accuracy: 0.8018 - val_loss: 0.4290
+# Epoch 37/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 21s 44ms/step - accuracy: 0.8200 - loss: 0.3995 - val_accuracy: 0.7990 - val_loss: 0.4303
+# Epoch 38/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 20s 44ms/step - accuracy: 0.8180 - loss: 0.4017 - val_accuracy: 0.7930 - val_loss: 0.4333
+# Epoch 39/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 14s 45ms/step - accuracy: 0.8185 - loss: 0.4003 - val_accuracy: 0.8032 - val_loss: 0.4283
+# Epoch 40/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 20s 43ms/step - accuracy: 0.8125 - loss: 0.4068 - val_accuracy: 0.8018 - val_loss: 0.4300
+# Epoch 41/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 14s 43ms/step - accuracy: 0.8164 - loss: 0.4008 - val_accuracy: 0.8012 - val_loss: 0.4290
+# Epoch 42/100
+# 313/313 ━━━━━━━━━━━━━━━━━━━━ 20s 43ms/step - accuracy: 0.8195 - loss: 0.4040 - val_accuracy: 0.8030 - val_loss: 0.4319    
